8d9fee55183ee9bbbd80e294d6df18a81601843a
nakyungLee20 <lnktop@snu.ac.kr>
2025-01-11 23:23:56 +0900

    nakyung 1123

--- src/AImodel.ts
+++ src/AImodel.ts
@@ -15,6 +15,8 @@ const bedrockClient = new BedrockRuntimeClient({ region: 'us-east-1' });
 // AWS Bedrock 함수
 async function callBedrockModel(prompt: string): Promise<string> {
   const modelId = 'anthropic.claude-3-5-sonnet-20241022-v2:0';
+  const inferenceProfile =
+    'arn:aws:bedrock:us-east-1:730335373015:inference-profile/us.anthropic.claude-3-5-sonnet-20241022-v2:0';
 
   const payload = {
     anthropic_version: 'bedrock-2023-05-31',
@@ -29,6 +31,7 @@ async function callBedrockModel(prompt: string): Promise<string> {
 
   const input = {
     modelId,
+    inferenceProfile,
     body: JSON.stringify({ payload }),
     contentType: 'application/json',
   };
