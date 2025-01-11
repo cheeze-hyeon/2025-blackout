5e5590f987cb6cc3c6f12fd61c0e655a14d4154a
최주현 <wngus0223@naver.com>
2025-01-12 04:40:54 +0900

    feat: 오늘의 회화 s3 데이터 리드 추가

--- src/today/index.ts
+++ src/today/index.ts
@@ -2,6 +2,7 @@ import { boltApp } from '../index';
 import { Logger, BlockAction, SlashCommand } from '@slack/bolt'; // Command 제거
 import { WebClient } from '@slack/web-api';
 import { requestConvers } from '../AImodel';
+import { getFromS3 } from '../s3service';
 
 export const registerTodayConversationEvents = async () => {
   // '/today' 명령어 핸들러 등록
@@ -17,8 +18,18 @@ export const registerTodayConversationEvents = async () => {
         return;
       }
 
-      // TO DO: S3 연결 완료 후 국가 정보 가져오기
-      const country = 'korea';
+      // TO DO: 어드민 워크스페이스 국가 정보 가져오기
+      const dataFromS3 = await getFromS3(userId);
+
+      // Convert Buffer to JSON string and parse it
+      const userInfo = JSON.parse(dataFromS3.toString('utf-8'));
+
+      console.log('userInfo', userInfo);
+
+      // Retrieve country information
+      const country = userInfo?.nationality ?? 'america';
+
+      console.log('Country:', country);
 
       const todayConversations = await requestConvers(country);
 
@@ -26,7 +37,7 @@ export const registerTodayConversationEvents = async () => {
 
       await client.chat.postMessage({
         channel: 'C0882E5KPU6',
-        text: `오늘의 회화\n${todayConversations}`,
+        text: `*오늘의 회화(Today's Phrase) 🗣️*\n\n${todayConversations}`,
       });
     } catch (error) {
       logger.error('오늘의 회화 중 오류 발생:', error);
