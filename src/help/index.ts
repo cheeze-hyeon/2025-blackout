ada31fc2227710864fe3727862234d6bead4b6aa
Rokhee LEE <rockey020312@snu.ac.kr>
2025-01-12 08:11:48 +0900

    fix: help

--- src/help/index.ts
+++ src/help/index.ts
@@ -11,15 +11,23 @@ export function registerHelpCommand(app: App) {
     await ack();
 
     const helpMessage = `
-*Commands:*
-- \`/globee_start\` : 사용자 정보를 다시 입력할 수 있습니다.
-- \`/trade\`: 트레이드를 진행할 수 있습니다.
-- \`/today\`: 오늘의 회화를 제공합니다.
+*HOW TO USE GLOBEE🐝*
 
-*Else:*
-- Translation: 번역하고 싶은 문장을 번역하고 싶은 국가의 국기로 설정하면 됩니다.
+- \`/help\`
+    GloBee🐝 도움말을 출력합니다.
 
-*If you need English help, please push an emoji 🇺🇸 at this comment.*
+- \`/globee_start\`
+    사용자 정보를 다시 입력할 수 있습니다.
+
+- \`/today\`
+    *#오늘의-회화* 채널에 지정된 국가의 오늘의 회화 구문을 제공합니다.
+
+- \`/trade\`
+    *#물건-팝니다-삽니다* 채널에 새로운 거래를 등록할 수 있습니다.
+
+- 번역
+    번역하고 싶은 문장에 국기(🇺🇸, 🇯🇵)로 감정표현을 표시하면, 번역이 제공됩니다. 
+    *If you need help by other langauges, please push an emoji(🇺🇸, 🇯🇵) at this comment.*
     `;
 
     try {
