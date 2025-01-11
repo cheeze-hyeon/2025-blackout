f22113860576aba627aeab0c364799ced0a32d2f
Taehyeungkim <kth1837@snu.ac.kr>
2025-01-12 05:27:07 +0900

    feat: update url logic

---
+++ src/crawling/index.ts
@@ -0,0 +1,72 @@
+import { chromium } from 'playwright';
+import {
+  getDocumentTextOfUrl,
+  getNextPageLinkByClickA,
+  getNextPageLinkByClickButton,
+} from './utils';
+
+export const getNextUrlToBegin = async (entryUrl: string) => {
+  const browser = await chromium.launch({ headless: true });
+  const page = await browser.newPage();
+  page.setDefaultNavigationTimeout(10000);
+  await page.goto(entryUrl);
+  // await page.waitForEvent('load');
+  const { textContent: entryTextContent } = await getDocumentTextOfUrl(
+    entryUrl,
+    page,
+  );
+  console.log('get entry url');
+
+  const nextLinkByA = await getNextPageLinkByClickA(page, browser);
+  if (nextLinkByA) {
+    await page.goto(nextLinkByA);
+    const { url, textContent } = await getDocumentTextOfUrl(nextLinkByA, page);
+    await browser.close();
+    console.log('get a', url, entryUrl);
+    const result = [
+      { link: url, text: textContent },
+      { link: entryUrl, text: entryTextContent },
+    ];
+
+    return result;
+  }
+  const nextLinkByButton = await getNextPageLinkByClickButton(page, browser);
+  if (nextLinkByButton) {
+    await page.goto(nextLinkByButton);
+    const { url, textContent } = await getDocumentTextOfUrl(
+      nextLinkByButton,
+      page,
+    );
+    console.log('get button', url, entryUrl);
+    await browser.close();
+    console.log(url, entryUrl);
+    const result = [
+      { link: url, text: textContent },
+      { link: entryUrl, text: entryTextContent },
+    ];
+    return result;
+  }
+  await browser.close();
+  return;
+};
+
+const crawlSite = async (url: string, recursive: boolean = true) => {
+  const browser = await chromium.launch({ headless: true });
+  const page = await browser.newPage();
+
+  await page.goto(url);
+
+  await page.waitForEvent('load');
+
+  const pageContent = await page.evaluate(() => {
+    return document.body.innerText;
+  });
+
+  console.log(pageContent);
+
+  await browser.close();
+};
+
+// 시작 URL을 입력
+// crawlSite('https://www.snu.ac.kr/');
+getNextUrlToBegin('https://oia.snu.ac.kr/visit-oia');
---
+++ src/crawling/utils/index.ts
@@ -0,0 +1,49 @@
+import { Browser, Page } from 'playwright';
+
+export const getDocumentTextOfUrl = async (url: string, page: Page) => {
+  console.log(url);
+  const textContent = await page.evaluate(() => document.body.innerText);
+  return { url, textContent };
+};
+
+export const getNextPageLinkByClickButton = async (
+  page: Page,
+  browser: Browser,
+) => {
+  console.log('button search');
+  const fallbackUrl = page.url();
+  const allButtons = await page.$$('button');
+  try {
+    if (allButtons.length !== 0) {
+      await allButtons[
+        Math.min(Math.floor(Math.random() * 10), allButtons.length - 1)
+      ].click();
+      await page.waitForLoadState('load');
+      return page.url();
+    }
+  } catch (e) {
+    return fallbackUrl;
+  }
+
+  return;
+};
+
+export const getNextPageLinkByClickA = async (page: Page, browser: Browser) => {
+  console.log('a search');
+  const fallbackUrl = page.url();
+  const allA = await page.$$('a');
+  if (allA.length !== 0) {
+    try {
+      const target =
+        allA[Math.min(Math.floor(Math.random() * 10), allA.length - 1)];
+      const targetHref = await target.evaluate((target) => target.href);
+
+      if (targetHref === fallbackUrl) return;
+      const url = targetHref;
+      return url;
+    } catch (e) {
+      return;
+    }
+  }
+  return;
+};
